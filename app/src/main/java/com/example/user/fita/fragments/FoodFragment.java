35408c12c64ff0aa7047509f5cb330113ceac215
emine <eminesa34@gmail.com>
2018-03-10 17:33:12 +0300

    isimler duzelditli

--- app/src/main/java/com/example/user/fita/fragments/FoodFragment.java
+++ app/src/main/java/com/example/user/fita/fragments/FoodFragment.java
@@ -9,23 +9,20 @@ import android.view.ViewGroup;
 
 import com.example.user.fita.R;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class GrammerFragment extends Fragment {
+public class FoodFragment extends Fragment {
 
 
-    public GrammerFragment() {
-        // Required empty public constructor
+    public FoodFragment() {
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grammer, container, false);
+
+        return inflater.inflate(R.layout.fragment_food, container, false);
+
+
     }
 
 }
